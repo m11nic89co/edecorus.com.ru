5e7e0c9ac8ca6601411f72fe0f9adad3516f8d7c
m11nic89co <58000724+m11nic89co@users.noreply.github.com>
2025-03-08 23:45:42 +0200

    44

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default HomePage;};    );        </div>            <Footer />            // ...existing code...        <div>    return (const HomePage = () => {import Footer from '../components/Footer';import React from 'react';
